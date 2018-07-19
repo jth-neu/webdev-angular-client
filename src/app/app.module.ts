6ede3dc74b8f329ec20981becac9cc837ed1510e
Jingtian Huang <huang.jin@husky.neu.edu>
2018-07-19 16:27:01 -0700

    Follow lecture slides

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HelloWorldComponent } from './hello-world.component';
 
 import { AppComponent } from './app.component';
+import { HelloWorldTwoComponent } from './hello-world-two/hello-world-two.component';
+import {FormsModule} from '@angular/forms';
+import {CourseNavigatorServiceClient} from '../services/course-navigator.service.client';
+import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    HelloWorldTwoComponent,
+    CourseNavigatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CourseNavigatorServiceClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/course-navigator/course-navigator.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {CourseNavigatorServiceClient} from '../../services/course-navigator.service.client';
+
+@Component({
+  selector: 'app-course-navigator',
+  templateUrl: './course-navigator.component.html',
+  styleUrls: ['./course-navigator.component.css']
+})
+export class CourseNavigatorComponent implements OnInit {
+
+  constructor(private service:
+                CourseNavigatorServiceClient
+  ) {
+  }
+
+  courses = [];
+  modules = [];
+  selectedCourseId = 0;
+  selectedModuleId = 0;
+
+  ngOnInit() {
+    this.service.findAllCourses()
+      .then(courses => {
+        this.courses = courses;
+      });
+  }
+  selectCourse(courseId) {
+    this.selectedCourseId = courseId;
+    this.service.findModulesForCourse(courseId)
+      .then(modules =>
+        this.modules = modules);
+  }
+  selectModule(moduleId) {
+    this.selectedModuleId = moduleId;
+  }
+
+
+}
