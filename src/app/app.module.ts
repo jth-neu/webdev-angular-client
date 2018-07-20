3183df26b4d328d36cafda1ecd5db50f550131b6
Jingtian Huang <huang.jin@husky.neu.edu>
2018-07-19 18:23:23 -0700

    Add course grid to white board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,29 @@ import { HelloWorldComponent } from './hello-world.component';
 import { AppComponent } from './app.component';
 import { HelloWorldTwoComponent } from './hello-world-two/hello-world-two.component';
 import {FormsModule} from '@angular/forms';
-import {CourseNavigatorServiceClient} from '../services/course-navigator.service.client';
+import {CourseNavigatorServiceClient} from './services/course-navigator.service.client';
 import { CourseNavigatorComponent } from './course-navigator/course-navigator.component';
+import { WhiteBoardComponent } from './white-board/white-board.component';
+import {CourseServiceClient} from './services/course.service.client';
+import { CourseGridComponent } from './course-grid/course-grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloWorldComponent,
     HelloWorldTwoComponent,
-    CourseNavigatorComponent
+    CourseNavigatorComponent,
+    WhiteBoardComponent,
+    CourseGridComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [CourseNavigatorServiceClient],
+  providers: [
+    CourseNavigatorServiceClient,
+    CourseServiceClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
