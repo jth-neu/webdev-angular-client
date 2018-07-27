44dc61196f3236bcb265030ad1be02676d6b31bd
Jingtian Huang <huang.jin@husky.neu.edu>
2018-07-27 12:38:47 -0700

    Show enrolled courses on home page

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseServiceClient} from '../services/course.service.client';
 import {Course} from '../models/course.model.client';
+import {SectionServiceClient} from '../services/section.service.client';
+import {UserServiceClient } from '../services/user.service.client';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -10,13 +13,47 @@ import {Course} from '../models/course.model.client';
 })
 export class CourseGridComponent implements OnInit {
 
-  constructor(private service: CourseServiceClient) { }
+  constructor(private courseService: CourseServiceClient,
+              private sectionService: SectionServiceClient,
+              private userService: UserServiceClient,
+              private router: Router) { }
 
   courses: Course[] = [];
+  loggedIn = false;
+  enrolledCourses: Course[] = [];
+  enrolledCourseIds = [];
+  enrolledSections = [];
 
   ngOnInit() {
-    this.service.findAllCourses()
+    this.courseService.findAllCourses()
       .then(courses => this.courses = courses);
+
+    this.userService.profile()
+      .then(res => {
+        return res._id;
+    })
+      .then((userId) => {
+        if (userId !== null) {
+          this.loggedIn = true;
+          this.sectionService.findSectionsForStudent()
+            .then(sections => {
+              this.enrolledSections = sections;
+              this.findEnrolledCourses();
+            });
+        }
+      });
+  }
+  findEnrolledCourses() {
+    this.enrolledSections.forEach((enrolledSection) => {
+      this.courseService.findCourseById(enrolledSection.section.courseId)
+        .then(course => {
+          const flag = !this.enrolledCourseIds.includes(course.id);
+          if (flag) {
+            this.enrolledCourseIds.push(course.id);
+            this.enrolledCourses.push(course);
+          }
+        });
+    });
   }
 
 }
